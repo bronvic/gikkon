4e6bc1037cd01a26805ab3219e7b3a68b70c7fd1
Voronwe <mail@voronwe.me>
2023-04-23 13:28:44 +0300

    Tests for --ask_rollback

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,3 @@
 import sys
-from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parent.parent))
+sys.path.append("src")
--- tests/test_wrappers.py
+++
@@ -1,7 +0,0 @@
-from unittest import SkipTest, TestCase
-
-
-class TestWrapper(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.skipTest(cls, "this wrappers would be refactored")
